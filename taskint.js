f1e1baeaa9bb1ec59e1afa889b1ea1baf76f1857
newbyca <newbyca@gmail.com>
2014-05-01 16:37:43 -0400

    Add lists

--- taskint.js
+++ taskint.js
@@ -1,17 +1,76 @@
+username = "newbyca";
+Users = new Meteor.Collection('users');
+Lists = new Meteor.Collection('lists');
 Tasks = new Meteor.Collection('tasks');
   
 if (Meteor.isClient) {
 
-  Template.main.tasks = function () {
-    return Tasks.find({}, {sort: {sort: 1, date: -1}});
+  Template.home.lists = function () {
+    return Lists.find({owner: username}, {sort: {sort: 1, date: -1}});
   };
 
-  Template.main.rendered = function(){
+  Template.home.rendered = function(){
+    var text = $('input[type=text]');
+    var lists = $('#lists');
+    text.keypress(function(e){
+      if(e.which == 13){
+        addList();
+      }
+    });
+    lists.sortable({
+      opacity: 0.75,
+      placeholder: 'placeholder',
+      update: function(e, o){
+        var lis = $('#lists li');
+        for(var i=0, max=lis.length; i < max; i++){
+          var li = $(lis[i]);
+          var id = li.attr('id');
+          Lists.update({'_id': id}, {$set: {sort: i}});
+        }
+      }
+    });
+    text.focus();
+  };
+
+  function addList(){
+    var text = $('input[type=text]');
+    var list = {
+      owner: username,
+      text: text.val(),
+      date: new Date(),
+      sort: 0
+    };
+    Lists.insert(list);
+    text.val('');
+  }
+
+  Template.list.events({
+    'click li .actions span:nth-child(1)': function (e, template) {
+      var list = this;
+      if(!list.fixed){
+        $(template.find('li')).fadeOut('fast', function(){
+          Tasks.find({list: list._id}).forEach(function(task){
+            Tasks.remove(task._id);
+          });
+          Lists.remove(list._id);
+        });
+      }
+    }
+  });
+
+  Template.tasklist.tasks = function () {
+    return Tasks.find(
+      {list: this._id},
+      {sort: {sort: 1, date: -1}}
+    );
+  };
+
+  Template.tasklist.rendered = function(){
     var text = $('input[type=text]');
     var tasks = $('#tasks');
     text.keypress(function(e){
       if(e.which == 13){
-        add();
+        addTask();
       }
     });
     tasks.sortable({
@@ -29,11 +88,14 @@ if (Meteor.isClient) {
     text.focus();
   };
 
-  function add(){
+  function addTask(){
+    var list = $('#list').val();
     var text = $('input[type=text]');
     var task = {
+      list: list,
       text: text.val(),
-      date: new Date()
+      date: new Date(),
+      sort: 0
     };
     Tasks.insert(task);
     text.val('');
@@ -52,6 +114,40 @@ if (Meteor.isClient) {
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
+    var firstUser = Users.findOne({username: username});
+    if(firstUser == null){
+
+      Users.insert({username: username});
+
+      var firstList = {
+        owner: username,
+        text: username,
+        fixed: true,
+        sort: 0,
+        date: new Date()
+      };
+      Lists.insert(firstList, function(e, id){
+        firstList._id = id;
+        Tasks.insert({list: id, text: 'Take', sort: 0});
+        Tasks.insert({list: id, text: 'My', sort: 1});
+        Tasks.insert({list: id, text: 'Wife', sort: 2});
+      });
+    }
   });
 }
+
+Router.map(function() {
+  this.route('home', {path: '/'})
+  this.route('tasklist', {
+    path: '/:username',
+    data: function(){
+      return Lists.findOne({owner: this.params.username, text: this.params.username});
+    }
+  })
+  this.route('tasklist', {
+    path: '/:username/:listname',
+    data: function(){
+      return Lists.findOne({owner: this.params.username, text: this.params.listname});
+    }
+  })
+});
